f86cab0fe02938b4b9da14a583448761ef3c21e1
Erick García de la Rosa <erick.garcia@cfe.mx>
2024-11-04 23:12:56 -0600

    Avance Front Relacionar Invitado con Actor

---
+++ APP.GP.Web/Controllers/InvitacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APP.GP.Web.Controllers
+{
+    public class InvitacionController : Controller
+    {
+        public IActionResult Relation()
+        {
+            return View();
+        }
+    }
+}
